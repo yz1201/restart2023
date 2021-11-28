b3e7d5df4c4b3a73dc0e139aaae7448d69cc0b20
yz1201 <15957121194@163.com>
2021-11-28 16:40:14 +0800

    test commit

---
+++ src/main/java/cn/dbdj1201/restart2022/Restart2022Application.java
@@ -0,0 +1,13 @@
+package cn.dbdj1201.restart2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restart2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Restart2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/dbdj1201/restart2022/Restart2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.dbdj1201.restart2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Restart2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
