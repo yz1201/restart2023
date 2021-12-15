cfcb3d4e93d2b971f3f60458210bc806d0e5121e
yz1201 <15957121194@163.com>
2021-12-15 20:01:37 +0800

    tired

--- src/main/java/cn/dbdj1201/restart2022/controller/TestController.java
+++ src/main/java/cn/dbdj1201/restart2022/controller/TestController.java
@@ -1,7 +1,9 @@
 package cn.dbdj1201.restart2022.controller;
 
 import cn.dbdj1201.restart2022.entity.TestEntity;
+import cn.dbdj1201.restart2022.mapper.TestUserMapper;
 import cn.dbdj1201.restart2022.service.ITestService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +22,9 @@ public class TestController {
     @Autowired
     private ITestService testService;
 
+    @Autowired
+    private TestUserMapper testUserMapper;
+
     @GetMapping("/listAll")
     public List<TestEntity> selectAllUsers() {
         log.info("test hot swap");
@@ -33,13 +38,21 @@ public class TestController {
     }
 
     @GetMapping("/{id}")
-    public TestEntity getUserById(@PathVariable Long id){
+    public TestEntity getUserById(@PathVariable Long id) {
         return this.testService.selectUserById(id);
     }
 
     @GetMapping("/all")
     public List<TestEntity> listAllUsers() {
-
         return this.testService.selectUserList();
+    }
+
+    @GetMapping("/age/{minAge}/{maxAge}")
+    public List<TestEntity> getUserByAge(@PathVariable int minAge, @PathVariable int maxAge) {
+        log.info("minAge - {}, maxAge - {}", minAge, maxAge);
+        QueryWrapper<TestEntity> queryWrapper = new QueryWrapper<>();
+        String ageStr = "age";
+        queryWrapper.ge(ageStr, minAge).le(ageStr, maxAge);
+        return this.testUserMapper.selectList(queryWrapper);
     }
 }
